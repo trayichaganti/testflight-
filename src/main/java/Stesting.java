1707adbac3ac0e576eb8ecd530fc043a3fb81bef
trayichaganti <trayivannu@gmail.com>
2022-04-22 22:28:15 +0200

    jg

--- src/main/java/Stesting.java
+++ src/main/java/Stesting.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Stesting {
+public class Stesting {
+
+    //AUTHOR: Ankhit Tangella
+    //Description: This function merges two strings. This function is tested by unit tests testmerge1(),testmerge2(),testmerge3()
+    public static String merge(String s1, String s2)
+    {
+        StringBuilder result = new StringBuilder();
+        if (s1== ""||s2== "")
+            return "error";
+        for (int i = 0; i < s1.length() || i < s2.length(); i++) {
+            if (i < s1.length())
+                result.append(s1.charAt(i));
+            if (i < s2.length())
+                result.append(s2.charAt(i));
+        }
+        return result.toString();
+    }
+    public static boolean palindrome(int num) {
+        String str = String.valueOf(num);
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i++) != str.charAt(j--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    static int search(int arr[], int n, int x)
+    {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
 }
--- src/test/java/StestingTest.java
+++ src/test/java/StestingTest.java
@@ -1,4 +1,69 @@
-import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
 public class StestingTest {
-  
+    public static Stesting obj;
+
+    @BeforeClass
+    public static void setup() {
+        obj = new Stesting();
+    }
+
+    //Author: Ankhit Tangella
+    //Description: This test case tests the merge function
+    @Test
+    public void testmerge1() {
+        assertEquals("Staomy", obj.merge("Sam", "toy"));
+    }
+
+    @Test
+    public void testmerge2() {
+        assertEquals("StAoMy", obj.merge("SAM", "toy"));
+    }
+
+    @Test
+    public void testmerge3() {
+        assertEquals("error", obj.merge("", "toy"));
+    }
+
+    @Test
+    public void testpalindrome1() {
+        assertEquals(true, obj.palindrome(121));
+
+
+    }
+
+    @Test
+    public void testpalindrome2() {
+        assertEquals(false, obj.palindrome(123));
+    }
+
+    @Test
+    public void testpalindrome3() {
+        assertEquals(true, obj.palindrome(111));
+    }
+
+    @Test
+    public void testsearch1() {
+        int arr[] = {1, 2, 3, 4};
+        assertEquals(-1, obj.search(arr, 4, 5));
+    }
+
+    @Test
+    public void testsearch2() {
+        int arr[] = {1, 2, 3, 4};
+        assertEquals(0, obj.search(arr, 4, 1));
+    }
+
+    @Test
+    public void testsearch3() {
+        int arr[] = {1, 2, 3, 4};
+        assertEquals(3, obj.search(arr, 4, 4));
+    }
 }
+
+
