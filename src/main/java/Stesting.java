0e33ae8951bf461319d5bec7775688aac03f3542
trayichaganti <trayivannu@gmail.com>
2022-04-23 20:01:43 +0200

    Test cases success by Trayi

--- src/main/java/Stesting.java
+++ src/main/java/Stesting.java
@@ -1,23 +1,33 @@
 public class Stesting {
     //Author: Trayi Chaganti
-    //Description: This function is used to merge tqo strings.
-
-    public static String merge(String s1, String s2)
-    {
+    //Description: This function is used to merge two strings
+    public static String merge(String s1, String s2) {
         StringBuilder result = new StringBuilder();
-        if(s1== "" ||s2== "")
+        if (s1 == "" || s2 == "")
             return "error";
-        for (int i = 0; i < s1.length() || i < s2.length(); i++){
+        for (int i = 0; i < s1.length() || i < s2.length(); i++) {
             if (i < s1.length())
                 result.append(s1.charAt(i));
             if (i < s2.length())
                 result.append(s2.charAt(i));
         }
         return result.toString();
+
     }
 
-
+    public static boolean palindrome(int num) {
+        String str = String.valueOf(num);
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i++) != str.charAt(j--)) {
+            }
+        }
+        return true;
+    }
 }
+
+
 
 
 
--- src/test/java/StestingTest.java
+++ src/test/java/StestingTest.java
@@ -13,14 +13,24 @@ public class StestingTest {
         obj = new Stesting();
     }
 
-    //Author: Trayi Chaganti
-    //Description: This test case is used to merge two strings.
+    //Author: Trayi chaganti
+    //Description: This test case is used to test merge two strings function
     @Test
     public void testmerge1(){
         assertEquals("Staomy", obj.merge("Sam", "toy"));
 
+
+
+    }
+    //Author: Trayi Chaganti
+    // Description: This test case is used to test the palindrome funtion
+    @Test
+    public void testpalindrome1(){
+        assertEquals(true, obj.palindrome(121));
+
     }
 }
+
 
 
 
