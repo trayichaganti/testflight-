15a659c7ba2f10a62aee49a3de0cffafd8b187d9
Y-Bhavya <70648992+Y-Bhavya@users.noreply.github.com>
2022-04-25 19:45:29 +0530

    All test cases

--- src/test/java/StestingTest.java
+++ src/test/java/StestingTest.java
@@ -16,45 +16,129 @@ public class StestingTest
         assertNotEquals(obj, null);
     }
 
-    //Author: Trayi chaganti
-    //Description: This test case is used to test the merging strings function
-    @Test
-    public void testmerge1(){
-        assertEquals("Staomy", obj.merge("Sam", "toy"));
+   //Author: Trayi chaganti
+    //Description: This test case is used to test merge two strings function@Test
+    public void testpalindrome2() {
+        assertEquals(false, obj.palindrome(123));
     }
+
     //Author: Trayi chaganti
-    //Description: This test case is used to test the palindrome function
-    @Test
-    public void testpalindrome1(){
-        assertEquals("yes",obj.palindrome(121))
-        ;
+    //Description: This test case is used to test merge two strings function@Test
+    public void testpalindrome3() {
+        assertEquals(true, obj.palindrome(111));
     }
 
     //Author: Ankhit Tangella
     //Description: This test case is used to test the length of the string function
     @Test
-    public void testlength1(){
-        assertEquals("7" , obj.LengthString("testing"));
+    public void testlength1() {
+        assertEquals("7", obj.LengthString("testing"));
+    }
+
+    //Author: Ankhit Tangella
+    //Description: This test case is used to test the length of the string function
+    @Test
+    public void testlength2() {
+        assertEquals("3", obj.LengthString("hii"));
+    }
+
+    //Author: Ankhit Tangella
+    //Description: This test case is used to test the length of the string function
+    @Test
+    public void testlength3() {
+        assertEquals("4", obj.LengthString("test"));
     }
 
     //Author: Ankhit Tangella
     //Description: This test case is used to test the reverse string function
     @Test
-    public void testreverse1(){
-        assertEquals("hi" , obj.ReverseString("ih"));
+    public void testreverse1() {
+        assertEquals("hi", obj.ReverseString("ih"));
     }
-    //Author: Bhavya Yaddula
-    //Description: This test case is used to test the Uppercase count function
+
+    //Author: Ankhit Tangella
+    //Description: This test case is used to test the reverse string function
     @Test
-    public void testUpperCase1(){
-        assertEquals("2",obj.UppercaseCount("SalT"));
-        }
-    //Author: Bhavya Yaddula
-    //Description: This test case is used to test the Lowercase count funtcion
-    @Test
-    public void testLowerCase1(){
-        assertEquals("2",obj.LowercaseCount("SalT"));
+    public void testreverse2() {
+        assertEquals("ankhit", obj.ReverseString("tihkna"));
     }
+
+    //Author: Ankhit Tangella
+    //Description: This test case is used to test the reverse string function
+    @Test
+    public void testreverse3() {
+        assertEquals("nm", obj.ReverseString("mn"));
+    }
+
+    //Author: Bhavya Yaddula
+    //Description: This test case is used to test the uppercase count function
+    @Test
+    public void testUpperCase1() {
+        assertEquals("2", obj.UppercaseCount("SalT"));
+
+    }
+
+    //Author: Bhavya Yaddula
+    //Description: This test case is used to test the uppercase count function
+    @Test
+    public void testUpperCase2() {
+        assertEquals("0", obj.UppercaseCount("sal"));
+
+    }
+
+    //Author: Bhavya Yaddula
+    //Description: This test case is used to test the uppercase count function
+    @Test
+    public void testUpperCase3() {
+        assertEquals("3", obj.UppercaseCount("SAT"));
+
+    }
+
+    //Author: Bhavya Yaddula
+    //Description: this test case is used to test the lowercase count function
+    @Test
+    public void testLowerCase1() {
+        assertEquals("2", obj.LowercaseCount("SalT"));
+
+    }
+
+    //Author: Bhavya Yaddula
+    //Description: this test case is used to test the lowercase count function
+    @Test
+    public void testLowerCase2() {
+        assertEquals("0", obj.LowercaseCount("ST"));
+
+    }
+
+    //Author: Bhavya Yaddula
+    //Description: this test case is used to test the lowercase count function
+    @Test
+    public void testLowerCase3() {
+        assertEquals("3", obj.LowercaseCount("lll"));
+
+    }
+
+    //Author: Trayi Chaganti
+    //Description: This test case is used to test both reverse string and length of string functions
+    @Test
+    public void integrationtest1() {
+
+        assertEquals("4", obj.ReverseString(obj.LengthString("toys")));
+    }
+    // Author: Ankhit tangella
+    // Description: This test case is used to test both the uppercase count and reverse string functions
+    @Test
+    public void integrationtest2(){
+        assertEquals("2", obj.ReverseString((obj.UppercaseCount("SaT"))));
+    }
+    // Author: Bhavya Yaddula
+    // Description: This test case is used to test both the Uppercase and lowercase count functions
+    @Test
+    public void intergrationtest3(){
+        assertEquals("0",obj.UppercaseCount(obj.LowercaseCount("HIII")));
+    }
+
+
 }
 
 
